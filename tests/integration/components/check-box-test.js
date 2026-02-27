01c89fe0216b34bff80d803f4354bf295ca8b81d
Xichuan Wang <xichuan.wang@gmail.com>
2026-02-27 09:39:10 -0800

    add integration test for checkbox

--- tests/integration/components/check-box-test.js
+++ tests/integration/components/check-box-test.js
@@ -1,26 +1,54 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'dummy/tests/helpers';
-import { render } from '@ember/test-helpers';
+import { render, find, click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+
+async function renderCheckBox({ isIndeterminate = false, selectedItems = [], onCheckboxChange = () => {} } = {}) {
+  this.setProperties({ isIndeterminate, selectedItems, onCheckboxChange });
+  return await render(hbs`
+    <CheckBox 
+      @isIndeterminate={{this.isIndeterminate}} 
+      @selectedItems={{this.selectedItems}} 
+      @onCheckboxChange={{this.onCheckboxChange}}
+    />
+  `);
+}
 
 module('Integration | Component | check-box', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+    this.set('onCheckboxChange', () => {});
 
-    await render(hbs`<CheckBox />`);
+    await renderCheckBox.call(this);
+    assert.dom(this.element).exists();
+  });
 
-    assert.dom(this.element).hasText('');
+  test('it shows "None selected" when no items are selected', async function (assert) {
+    await renderCheckBox.call(this, { selectedItems: [] });
+    assert.dom(this.element).hasTextContaining('None selected');
+  });
 
-    // Template block usage:
-    await render(hbs`
-      <CheckBox>
-        template block text
-      </CheckBox>
-    `);
+  test('it shows the number of selected items', async function (assert) {
+    const selectedItems = new Set(['item1', 'item2']);
+    await renderCheckBox.call(this, { selectedItems });
+    assert.dom(this.element).hasTextContaining('Selected 2');
+  });
 
-    assert.dom(this.element).hasText('template block text');
+  test('it shows indeterminate state', async function (assert) {
+    await renderCheckBox.call(this, { isIndeterminate: true });
+    const checkbox = find('input[type="checkbox"]');
+    assert.ok(checkbox.indeterminate, 'Checkbox should be in indeterminate state');
+  });
+
+  test('it calls onCheckboxChange when checkbox is changed', async function (assert) {
+    assert.expect(1);
+    const onCheckboxChange = () => {
+      assert.ok(true, 'onCheckboxChange should be called');
+    };
+
+    await renderCheckBox.call(this, { onCheckboxChange });
+    const checkbox = find('input[type="checkbox"]');
+    await click(checkbox);
   });
 });
